927b11e0477b06b62b103e805edf884b33b4e0f7
calexs <leopardotm@outlook.com>
2021-09-20 18:06:36 -0300

    Step Reuse Dialogs.

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,16 +1,21 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel"
-], function (UIComponent, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "./controller/HelloDialog"
+], function (UIComponent, JSONModel, HelloDialog) {
     "use strict";
+
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+
         metadata: {
-            interfaces: ["sap.ui.core.IAsyncContentCreation"],
-            manifest: "json"
+            manifest: "json",
+            interfaces: ["sap.ui.core.IAsyncContentCreation"]
         },
+
         init: function () {
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
+
             // set data model
             var oData = {
                 recipient: {
@@ -19,6 +24,24 @@ sap.ui.define([
             };
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
+
+            // set dialog, we wait for the
+            this.rootControlLoaded().then(function () {
+                this._helloDialog = new HelloDialog(this.getRootControl());
+            }.bind(this));
+        },
+
+        exit: function () {
+            if (this._helloDialog) {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            }
+        },
+
+        openHelloDialog: function () {
+            this._helloDialog.open();
         }
+
     });
+
 });
